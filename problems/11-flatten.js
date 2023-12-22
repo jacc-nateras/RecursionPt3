07b2613451e74fb59ea9da8c86d7028714795a3f
jacqueline nateras <jbree2017@gmail.com>
2023-12-21 16:53:11 -0800

    first

--- problems/11-flatten.js
+++ problems/11-flatten.js
@@ -13,9 +13,24 @@ flatten([1, [2, [3]]]); // [1, 2, 3]
 ***********************************************************************/
 
 function flatten(arr) {
-  // Your code here 
+  //if ( arr === []) return [];
+  let flattened = [];
+  for (let i = 0; i < arr.length; i++ ){
+   if (Array.isArray(arr[i])) {
+    flattened = flattened.concat(flatten(arr[i]));
+   } else {
+    flattened.push(arr[i]);
+   }
+  }
+   return flattened;
+
 }
-  
+
+// let flatten = arr.reduce((acc, currValue) => {
+//   return acc.concat(currValue);
+//}, []);
+console.log(flatten([])); // []
+console.log(flatten([1, 2])); // [1, 2]
+console.log(flatten([1, [2, [3]]])); // [1, 2, 3]
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = flatten;
-  
